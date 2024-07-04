8129c8de5bd46e32ae9a3ed19c5ede3a7e5482c2
sithabisenim <111012312+sithabisenim@users.noreply.github.com>
2024-07-04 23:02:13 +0200

    Create to_try.py

---
+++ to_try.py
@@ -0,0 +1,129 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime, timedelta
+import paramiko
+import boto3
+import os
+
+# DAG arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+dag = DAG(
+    'aws_data_pipeline',
+    default_args=default_args,
+    description='A simple data pipeline with AWS services',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+)
+
+# SSH and S3 details
+EC2_KEY_PATH = '/home/ubuntu/2401FTDE-SITMTS-ec2-de-mbd-predict-key.pem'
+EC2_HOST = 'ec2-52-17-182-138.eu-west-1.compute.amazonaws.com'
+EC2_USERNAME = 'ubuntu'
+MOUNT_SCRIPT = '/home/ubuntu/mount_s3_bucket.sh'
+PROCESSING_SCRIPT = '/mnt/s3/Scripts/data_processing.py'
+S3_BUCKET = '2401ft-mbd-predict-sithabiseni-mtshali-s3-source'
+SNS_TOPIC_ARN = 'arn:aws:sns:eu-west-1:445492270995:2401-mbd-predict-Sithabiseni-Mtshali-SNS'
+
+# Function to SSH to EC2 instance and run a command
+def ssh_and_run_script(ssh_host, ssh_user, ssh_key_path, script_path):
+    k = paramiko.RSAKey.from_private_key_file(ssh_key_path)
+    c = paramiko.SSHClient()
+    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    c.connect(ssh_host, username=ssh_user, pkey=k)
+    stdin, stdout, stderr = c.exec_command(f'bash {script_path}')
+    print(stdout.read().decode())
+    print(stderr.read().decode())
+    c.close()
+
+# Task to mount S3 bucket on EC2 instance
+def mount_s3():
+    ssh_and_run_script(EC2_HOST, EC2_USERNAME, EC2_KEY_PATH, MOUNT_SCRIPT)
+
+# Task to run data processing script on EC2 instance
+def run_processing_script():
+    ssh_and_run_script(EC2_HOST, EC2_USERNAME, EC2_KEY_PATH, PROCESSING_SCRIPT)
+
+# Task to move processed data from S3 to RDS
+def move_data_to_rds():
+    s3 = boto3.client('s3')
+    pg_hook = PostgresHook(postgres_conn_id='postgres_conn_id')  # Replace with your Postgres connection ID
+    conn = pg_hook.get_conn()
+    cursor = conn.cursor()
+
+    # Download the processed CSV file from S3
+    s3.download_file(S3_BUCKET, 'Output/processed_data.csv', '/tmp/processed_data.csv')
+
+    # Read the SQL insertion query from the S3 bucket
+    s3.download_file(S3_BUCKET, 'Scripts/insert_query.sql', '/tmp/insert_query.sql')
+    with open('/tmp/insert_query.sql', 'r') as query_file:
+        insert_query = query_file.read()
+
+    # Read the CSV file and insert data into RDS
+    with open('/tmp/processed_data.csv', 'r') as f:
+        cursor.copy_expert(insert_query, f)  # Use the query from the insert_query.sql file
+    conn.commit()
+    cursor.close()
+    conn.close()
+    os.remove('/tmp/processed_data.csv')
+    os.remove('/tmp/insert_query.sql')
+
+# Task to send notification via SNS
+def send_notification(message):
+    sns_client = boto3.client('sns')
+    sns_client.publish(
+        TopicArn=SNS_TOPIC_ARN,
+        Message=message
+    )
+
+# Define the tasks
+mount_s3_task = PythonOperator(
+    task_id='mount_s3',
+    python_callable=mount_s3,
+    dag=dag,
+)
+
+run_processing_task = PythonOperator(
+    task_id='run_processing_script',
+    python_callable=run_processing_script,
+    dag=dag,
+)
+
+move_data_task = PythonOperator(
+    task_id='move_data_to_rds',
+    python_callable=move_data_to_rds,
+    dag=dag,
+)
+
+notify_success_task = PythonOperator(
+    task_id='notify_success',
+    python_callable=send_notification,
+    op_kwargs={'message': 'Pipeline succeeded!'},
+    dag=dag,
+)
+
+notify_failure_task = PythonOperator(
+    task_id='notify_failure',
+    python_callable=send_notification,
+    op_kwargs={'message': 'Pipeline failed!'},
+    trigger_rule='one_failed',
+    dag=dag,
+)
+
+# Set task dependencies
+mount_s3_task >> run_processing_task >> move_data_task
+move_data_task >> notify_success_task
+mount_s3_task >> notify_failure_task
+run_processing_task >> notify_failure_task
+move_data_task >> notify_failure_task
